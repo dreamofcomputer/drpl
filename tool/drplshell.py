4d3de2ecbc337e0893a7169f1bf695d1cff56387
dreamofcomputer <nowasnd@qq.com>
2023-09-02 18:28:05 +0800

    rm newDrplItem and add drplshell

---
+++ tool/drplshell.py
@@ -0,0 +1,23 @@
+import os
+
+
+print("DRPLshell 1.0\ndreamofcomputer")
+
+while True:
+    text=input("ds>")
+    if "new" in text:
+        text=text.replace("new ","")
+        path=input("path>")
+        os.mkdir(path+"\\"+text)
+        os.system("copy drpl.py "+path+"\\"+text)
+        os.system("copy dlib.py "+path+"\\"+text)
+        os.system("copy drcode.txt "+path+"\\"+text)
+        os.system("copy runItem.bat "+path+"\\"+text)
+        print("new drpl item ok")
+    
+    if "run" in text:
+        text=text.replace("run ","")
+        os.system("start "+text+"\\runItem.bat")
+    
+    if text=="exit":
+        break
--- tool/newDrplitem.py
+++
@@ -1,9 +0,0 @@
-import os
-itemname=input("new drpl item name>")
-path=input("new drpl item path>")
-os.mkdir(path+"\\"+itemname)
-os.system("copy drpl.py "+path+"\\"+itemname)
-os.system("copy dlib.py "+path+"\\"+itemname)
-os.system("copy drcode.txt "+path+"\\"+itemname)
-os.system("copy runItem.bat "+path+"\\"+itemname)
-end=input("new drpl item ok")
